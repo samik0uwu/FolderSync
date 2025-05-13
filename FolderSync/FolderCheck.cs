fc2eda440cb1dfc936506ab5d9fb47d91cccf128
sam <sam.brchel@gmail.com>
2025-05-13 12:23:00 +0200

    Folder copy

--- FolderSync/FolderCheck.cs
+++ FolderSync/FolderCheck.cs
@@ -5,30 +5,30 @@ namespace FolderSync;
 
 public class FolderCheck
 {
-    public string source { get; private set; }
-    public string target { get; private set; }
+    public string sourcePath { get; private set; }
+    public string targetPath { get; private set; }
     public string interval { get; private set; } //should i use timespan?
     public string log { get; set; }
 
     public FolderCheck(string source, string target, string interval, string log)
     {
-        this.source = source;
-        this.target = target;
+        this.sourcePath = source;
+        this.targetPath = target;
         this.interval = interval;
         this.log = log;
     }
     
-    private (string[], string[]) GetSourceFiles()
+    private (string[], string[]) GetFiles(string filePath)
     {
-        if (Directory.Exists(source))
+        if (Directory.Exists(filePath))
         {
-            string[] files = Directory.GetFiles(source, "*", SearchOption.AllDirectories);
+            string[] files = Directory.GetFiles(filePath, "*", SearchOption.AllDirectories);
             foreach (var file in files)
             {
                 Console.WriteLine(file);
             }
 
-            string[] folders = Directory.GetDirectories(source, "*", SearchOption.AllDirectories);
+            string[] folders = Directory.GetDirectories(filePath, "*", SearchOption.AllDirectories);
             foreach (var folder in folders)
             {
                 Console.WriteLine(folder);
@@ -52,35 +52,46 @@ public class FolderCheck
     }
     public void CompareFiles()
     {
-        var (srcFiles, srcFolders) = GetSourceFiles();
+        var (srcFiles, srcFolders) = GetFiles(sourcePath);
 
+        //go through folders first
+        foreach (var srcFolder in srcFolders)
+        {
+            string relativeFolder=Path.GetRelativePath(sourcePath, srcFolder);
+            string targetFolder=Path.Combine(targetPath, relativeFolder);
+            Directory.CreateDirectory(targetFolder); //creates folder unless it exists
+            Console.WriteLine(targetFolder);
+        }
+        
+        
         foreach (var srcFile in srcFiles)
         {
-            string file = Path.GetFileName(srcFile);
-            //i need to get filename for the srcfile and then check it in replica
-            if (File.Exists(target+"\\"+file))
+            string file = Path.GetRelativePath(sourcePath, srcFile); //includes subfolders
+            if (File.Exists(targetPath+"\\"+file))
             {
                 //checksum
                 var srcHash= GetHash(srcFile);
-                var trgHash=GetHash(target+"\\"+file);
+                var trgHash=GetHash(targetPath+"\\"+file);
                 
                 
                 if (!srcHash.SequenceEqual(trgHash))
                 {
-                    File.Copy(srcFile, target+"\\"+file, overwrite:true);
-                    Console.WriteLine($"File {file} was updated in folder {target}");
+                    File.Copy(srcFile, targetPath+"\\"+file, overwrite:true);
+                    Console.WriteLine($"File {file} was updated in folder {targetPath}");
                     //replace
                 }
                 
             }
             else
             {
-                File.Copy(srcFile, target+"\\"+file);
-                Console.WriteLine($"File {file} was copied to folder {target}");
+                File.Copy(srcFile, targetPath+"\\"+file);
+                Console.WriteLine($"File {file} was copied to folder {targetPath}");
                //copy file 
             }
 
-            
+            var (trgFiles, trgFolders) = GetFiles(targetPath);
+
+
             
         }
         
